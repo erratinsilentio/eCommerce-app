2c55f1e48aa37864b1737bf0e84f5b8d800894d8
erratinsilentio <kacper.kondracki@wp.pl>
2022-10-14 19:02:46 +0200

    added simple svg to blank cart

--- src/components/Dropdowns/Cart.js
+++ src/components/Dropdowns/Cart.js
@@ -1,11 +1,12 @@
 import {React, PureComponent} from "react";
+import blank from "../../styles/svg/blank-face.svg"
 
 class Cart extends PureComponent{
 
     render(){
         return (
             <div className={this.props.className} style={{display: this.props.display}}>
-
+                <img src={blank}></img>
             </div>
         )
     }
